d9f3763d8b676ab03b477415b10e6633651a22ec
AlanAC20 <a326769@alumnos.uaslp.mx>
2025-10-04 22:25:39 -0600

    quité tabla duplicada 'encargados'

---
+++ database/migrations/2025_10_05_042231_drop_encargados_table.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void   { Schema::dropIfExists('encargados'); }
+    public function down(): void { /* no recreamos */ }
+};

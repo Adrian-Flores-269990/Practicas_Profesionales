07e7e300321a101f342e0558e1f236ece5e10dbc
AlanAC20 <a326769@alumnos.uaslp.mx>
2025-10-04 22:51:11 -0600

    borró (users/cache/jobs/etc.)

--- database/migrations/2025_10_05_042231_drop_encargados_table.php
+++ database/migrations/2025_10_05_042231_drop_encargados_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void   { Schema::dropIfExists('encargados'); }
+    public function up(): void   { Schema::disableForeignKeyConstraints();
+                                    Schema::dropIfExists('encargados');
+                                    Schema::enableForeignKeyConstraints(); }
     public function down(): void { /* vacío a propósito */ }
 };
